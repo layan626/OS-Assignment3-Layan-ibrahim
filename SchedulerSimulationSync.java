31b49b16b13e40cfd9cc423f9f0c12874ee0f0af
layan626 <445052113@std.psau.edu.sa>
2026-04-30 18:32:46 +0300

    Add context switch increment method with locking

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -63,10 +63,13 @@ class SharedResources {
    
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
-        // TODO: Protect this critical section with a lock
+       completedProcessLock.lock();
+        try{
         completedProcessCount++;
+         }finally {
+    completedProcessLock.unlock();
     }
-    
+}
     // Method to add waiting time
     public static void addWaitingTime(long time) {
         // TODO: Protect this critical section with a lock
